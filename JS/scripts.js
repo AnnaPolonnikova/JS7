37a33586170ba869e9194425a62451222ea76c33
Anna <Anna.polonnikova@list.ru>
2022-07-31 16:25:47 +0300

    commit2

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,24 @@
 let Contacts = function(){
    this.LastName = ' ',
-   this.name = ' ',
+   this.Name = ' ',
    this.FirstName = ' ',
-   this.age = 0,
-   this.phone = false;
+   this.Age = 0,
+   this.Phone = false;
 
 }
 this.newContact = function(){
     this.LastName = prompt ('Введите фамилию'),
     this.Name = prompt ('Введите Имя'),
     this.FirstName = prompt ('Введите отчество'),
-    this.age = +prompt ('Введите возраст'),
-    this.phone = +prompt ('Введите номер телефона')
+    this.Age = +prompt ('Введите возраст'),
+    this.Phone = +prompt ('Введите номер телефона')
+}
+
+this.age = function(){
+    if (age >= 0 && age <= 100){
+        document.write (`Возраст: %{age}`)
+    }
+}
+this.show = function (){
+    document.write (newContact)
 }
